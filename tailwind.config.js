432c71076c08be7aa2e108a5a4a8258580aa737e
YOLO-GIT <ahmadsufi345@gmail.com>
2024-07-15 01:20:06 +0800

    Update TW Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.{html,js}", "./dist/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
